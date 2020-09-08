0e4f1bac99667495cba1baa28b5d80d4e6709223
sp <sam.pack.sp@gmail.com>
2020-09-08 21:17:20 +0930

    put quiz questions into javscript objects

---
+++ script.js
@@ -0,0 +1,34 @@
+const questionOne = {
+    question: "Commonly used data types DO NOT include:",
+    answerone: "strings",
+    answertwo: "booleans",
+    answerthree: "alerts",
+    answerfour: "numbers", }
+
+const questionTwo = {
+    question: "The condition with an if/else statement is enclosed within _____",
+    answerone: "quotes",
+    answertwo: "curly brackets",
+    answerthree: "parentheses",
+    answerfour:  "square brackets",}    
+
+const questionTwo = {
+    question: "Arrays in javascript can be used to store:",
+    answerone: "numbers and strings",
+    answertwo: "other arrays",
+    answerthree: "booleans",
+    answerfour: "all of the above", }            
+
+const questionTwo = {
+    question: "String values must be closed within _______ when being assigned to variables.",
+    answerone: "commas",
+    answertwo: "curly brackets",
+    answerthree: "quotes",
+    answerfour: "parentheses",}                
+
+const questionTwo = {
+    question: "A very useful tool used during development and debugging for printing content ",
+    answerone: "javascript",
+    answertwo: "terminal/bash",
+    answerthree: "for loops",
+    answerfour: "console log", }                   
