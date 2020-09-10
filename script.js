0378ca91761c03fb186b2d64ef4429136dbe036a
sp <sam.pack.sp@gmail.com>
2020-09-10 17:58:12 +0930

    Got the questions and answers showing upon button click - the right answer/wrong answer is not working yet

--- script.js
+++ script.js
@@ -6,53 +6,19 @@ let answerThree = document.getElementById("btn3")
 let answerFour = document.getElementById("btn4")
 let quList = document.getElementById("qulist")
 let questionTitle = document.getElementById("title")
-
+let rightOrWrong = document.getElementById("right")
     
 
 const questionOne = {
-    title: "Question 1",
-    question: "Commonly used data types DO NOT include:",
-    answerone: "strings",
-    answertwo: "booleans",
-    answerthree: "alerts",
-    answerfour: "numbers", 
+    title: ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5",],
+    question: ["Commonly used data types DO NOT include:", "The condition with an if/else statement is enclosed within _____", "Arrays in javascript can be used to store:", "String values must be closed within _______ when being assigned to variables.", "A very useful tool used during development and debugging for printing content ",],
+    answerone: ["strings","quotes", "numbers and strings", "commas", "javascript",],
+    answertwo: ["booleans", "curly brackets", "other arrays", "terminal/bash",],
+    answerthree: ["alerts", "parentheses", "booleans", "quotes", "for loops",],
+    answerfour: ["numbers", "square brackets", "all of the above", "parentheses", "console log",]
 }
-
-const questionTwo = {
-    title: "Question 2",
-    question: "The condition with an if/else statement is enclosed within _____",
-    answerone: "quotes",
-    answertwo: "curly brackets",
-    answerthree: "parentheses",
-    answerfour:  "square brackets",
-}    
-
-const questionThree = {
-    title: "Question 3",
-    question: "Arrays in javascript can be used to store:",
-    answerone: "numbers and strings",
-    answertwo: "other arrays",
-    answerthree: "booleans",
-    answerfour: "all of the above", 
-}            
-
-const questionFour = {
-    title: "Question 4",
-    question: "String values must be closed within _______ when being assigned to variables.",
-    answerone: "commas",
-    answertwo: "curly brackets",
-    answerthree: "quotes",
-    answerfour: "parentheses",
-}                
-
-const questionFive = {
-    title: "Question 5",
-    question: "A very useful tool used during development and debugging for printing content ",
-    answerone: "javascript",
-    answertwo: "terminal/bash",
-    answerthree: "for loops",
-    answerfour: "console log", 
-}      
+        
+    
    
 
     startBtn.addEventListener('click', startQuiz);
@@ -62,25 +28,40 @@ const questionFive = {
         quList.classList.remove('hide');
         nextQuestion();
     };  
-      
+    
+    answerOne.addEventListener('click', rightWrong);
+    answerTwo.addEventListener('click', rightWrong);
+    answerThree.addEventListener('click', rightWrong);
+    answerFour.addEventListener('click', rightWrong);
+    
+    var rightCount = 0
+    
+    function rightWrong(){
+        if(innerText= "alerts" || "parentheses" || "all of the above" || "quotes" || "console log"){
+            rightCount++;
+            rightOrWrong.innerText = "Right Answer"
+        } else {rightOrWrong.innerText = "Wrong Answer"};
+
+        nextQuestion();
+    }
+    
+    var questionCount = 0
+    
     function nextQuestion(){
         showNextQuestion();
+        questionCount++
     };
 
     function showNextQuestion(){
-        questionTitle.innerText = questionOne['title'];
-        quizQuestion.innerText = questionOne['question'];
-        answerOne.innerText = questionOne['answerone'];
-        answerTwo.innerText = questionOne['answertwo'];
-        answerThree.innerText = questionOne['answerthree'];
-        answerFour.innerText = questionOne['answerfour'];
+        questionTitle.innerText = questionOne.title[questionCount];
+        quizQuestion.innerText = questionOne.question[questionCount];
+        answerOne.innerText = questionOne.answerone[questionCount];
+        answerTwo.innerText = questionOne.answertwo[questionCount];
+        answerThree.innerText = questionOne.answerthree[questionCount];
+        answerFour.innerText = questionOne.answerfour[questionCount];
     };
-   // }
 
-  //  function chooseAnswer(){
-
-  //  }
-
-  //  function countRight(){
-
-//}
+    if (questionCount >= 5){
+        questionTitle.innerText = "CONGRATULATIONS"
+        quizQuestion.innerText = "You've finished the Quiz."
+    }
