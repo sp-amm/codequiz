4a9998f969a35660bcc5e88e577796561b1fd271
sp <sam.pack.sp@gmail.com>
2020-09-10 10:29:21 +0930

    hide start button success

--- script.js
+++ script.js
@@ -1,46 +1,76 @@
+var startBtn = document.getElementById("start")
 let quizQuestion = document.getElementById("question")
 let answerOne = document.getElementById("a1")
 let answerTwo = document.getElementById("a2")
 let answerThree = document.getElementById("a3")
 let answerFour = document.getElementById("a4")
+let quList = document.getElementById("qulist")
+
     
+const starter = {
+    question: "Welcome to the javascript quiz. Press start and good luck.",
+    answerone: "Start",
+}
+
 const questionOne = {
     question: "Commonly used data types DO NOT include:",
     answerone: "strings",
     answertwo: "booleans",
     answerthree: "alerts",
-    answerfour: "numbers", }
+    answerfour: "numbers", 
+}
 
 const questionTwo = {
     question: "The condition with an if/else statement is enclosed within _____",
     answerone: "quotes",
     answertwo: "curly brackets",
     answerthree: "parentheses",
-    answerfour:  "square brackets",}    
+    answerfour:  "square brackets",
+}    
 
-const questionTwo = {
+const questionThree = {
     question: "Arrays in javascript can be used to store:",
     answerone: "numbers and strings",
     answertwo: "other arrays",
     answerthree: "booleans",
-    answerfour: "all of the above", }            
+    answerfour: "all of the above", 
+}            
 
-const questionTwo = {
+const questionFour = {
     question: "String values must be closed within _______ when being assigned to variables.",
     answerone: "commas",
     answertwo: "curly brackets",
     answerthree: "quotes",
-    answerfour: "parentheses",}                
+    answerfour: "parentheses",
+}                
 
-const questionTwo = {
+const questionFive = {
     question: "A very useful tool used during development and debugging for printing content ",
     answerone: "javascript",
     answertwo: "terminal/bash",
     answerthree: "for loops",
-    answerfour: "console log", }      
+    answerfour: "console log", 
+}      
+   
+startBtn.addEventListener('click', function(){
+    console.log("start");
+        startBtn.classList.add('hide');
+        quList.classList.remove('hide');
+    });  
+   
+//function startHide()
     
+   
+        
 
+    //function nextQuestion(){
 
-    function countRight(){
+   // }
 
-}
+  //  function chooseAnswer(){
+
+  //  }
+
+  //  function countRight(){
+
+//}
